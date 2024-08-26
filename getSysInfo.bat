885c580abaf9d9d632c565195b6edd0ce2f76212
alos-source <alexander.oswald@web.de>
2024-08-26 14:51:03 +0200

    add details for RAM

--- getSysInfo.bat
+++ getSysInfo.bat
@@ -9,7 +9,9 @@ wmic diskdrive get caption, status, serialnumber, Size >>%FILENAME%
 wmic os get version>>%FILENAME%
 
 wmic CPU get NumberOfCores,NumberOfLogicalProcessors>>%FILENAME%
-wmic memorychip get capacity>>%FILENAME%
+
+wmic memorychip get devicelocator, manufacturer, capacity, PartNumber, serialnumber, speed, memorytype, formfactor >>%FILENAME%
+
 wmic nic get AdapterType, Name, Installed, MACAddress, PowerManagementSupported, Speed>>%FILENAME%
 
 wmic computersystem get domain >>%FILENAME%
